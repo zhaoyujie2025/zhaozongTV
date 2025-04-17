cd0eff676ca4788bb33fba80fa307a78058a6fae
Ouonnki <ouonnki@outlook.jp>
2025-04-17 21:10:08 +0800

    更新 page.tsx 文件，移除原有的返回内容，添加重定向至 /index.html。

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Page() {
-  return (
-    <h1 className="text-3xl font-bold underline">Hello, Next.js!xiugai</h1>
-  );
+  redirect("/index.html");
 }

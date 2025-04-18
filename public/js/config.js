4e77936d923371beb5f0e2e993327dbcee14ba3d
wangenqi <wangenqi@xd.com>
2025-04-18 11:19:04 +0800

    将项目名称和相关链接从 LibreTV 更新为 OuonnkiTV，修改播放器页面标题和关于页面内容，以确保品牌一致性。

--- public/js/config.js
+++ public/js/config.js
@@ -7,8 +7,8 @@ const MAX_HISTORY_ITEMS = 5;
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: 'OuonnkiTV',
+    url: 'https://tv.ouonnki.site',
     description: '免费在线视频搜索与观看平台',
     logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
     version: '1.0.0'
